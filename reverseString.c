bc393a86e6668336401441d23aa60874507d646d
Danijel Jovanovic <36568690+DanijelXDA@users.noreply.github.com>
2018-03-06 09:14:55 +0100

    Initial version of program
    -> Initial code publish
         -> Added string load using scanf();
         -> Added string re-allocation
         -> Added console output to show new string sizes

---
+++ reverseString.c
@@ -0,0 +1,30 @@
+/*              reverseString                        */
+/* A C program which demonstrates string reversering */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(void)
+{
+    char string[500];
+    int strLen = 0;
+
+    printf("Unesite neki string: ");
+    scanf("%[^\n]", string);
+
+    strLen = strlen(string);
+
+    char reAllocatedString[strLen];
+
+    strcpy(reAllocatedString, string);
+
+
+    printf("%s", reAllocatedString);
+
+    printf("\n\nIzvorna velicina stringa: %d", sizeof(string));
+    printf("\n\nVelicina re-alociranog stringa: %d\n", sizeof(reAllocatedString));
+
+    return 0;
+}
