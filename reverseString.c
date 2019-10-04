792a014e2dd58da03c9ad56acdbdb644fbaa3f67
saurav kumar <55906348+saurav1423@users.noreply.github.com>
2019-10-05 00:49:34 +0530

    easy way to reverse string

--- reverseString.c
+++ reverseString.c
@@ -1,55 +1,21 @@
 /*              reverseString                        */
 /* A C program which demonstrates string reversering */
 
-
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-char *obrniString(char *, int);
-
-int main(void)
+int main()
 {
-    char string[500];
-    int strLen = 0;
-
-    printf("Unesite neki string: ");
-    scanf("%[^\n]", string);
-
-    strLen = strlen(string);
-
-    char reAllocatedString[strLen];
-
-    strcpy(reAllocatedString, string);
-
-
-    printf("%s", reAllocatedString);
-
-    printf("\n\nIzvorna velicina stringa: %d", sizeof(string));
-    printf("\n\nVelicina re-alociranog stringa: %d\n", sizeof(reAllocatedString));
-    
-    printf("\nObrnuti string je: \n");
-    printf("%s\n", obrniString(reAllocatedString, strLen));
-
-    
-    return 0;
+char str[1000], rev[1000];
+int i, j, count = 0;
+scanf("%s", str);
+while (str[count] != '\0')
+{
+count++;
 }
-
-char *obrniString (char *str, int strLen)
+j = count - 1;
+for (i = 0; i < count; i++) 
 {
-    int i;
-    char c;
-
-    if (!str)
-        return NULL;
-
-    for(i = 0; i < (strLen/2); i++)
-    {
-        c = str[i];
-        str [i] = str[strLen - i - 1];
-        str[strLen - i - 1] = c;
-    }
-
-    return str;
-
+rev[i] = str[j];
+j--;
+}
+printf(rev);
 }
